58fbceaf12dc983c5e349b6e51f33ecbcc0ba8cd
paulasofiagc <paulasofiagc@gmail.com>
2022-11-16 00:50:08 -0300

    Terminando página de inicio

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
 import "../styles/init.css";
- import logo from '../img/logo-burger-joint.png'
+import logo from "../img/logo-burger-joint.png";
+import burgerOrden from "../img/burger-inicio.png";
 export const Homepage = () => {
   return (
     <div className="home-container">
-        <nav>
-        <img src={logo} />
+      <nav>
+        <img src={logo} alt="logo"/>
         <button className="menu-btn">Menú</button>
-        </nav>
+      </nav>
       <section className="buttons-container">
         <Link to="order">
-          <button className="orderBtn">Botonsito orden</button>
+          <button className="orderBtn">
+          <img src={burgerOrden} className="burger-inicio"alt="burger"></img>New Order</button>
         </Link>
         <Link to="kitchen">
-          <button className="kitchenBtn">Botonsito Cocina</button>
+          <button className="kitchenBtn">
+            <img src={burgerOrden} className="burger-inicio"alt="burger"></img>Kitchen
+          </button>
         </Link>
       </section>
     </div>
